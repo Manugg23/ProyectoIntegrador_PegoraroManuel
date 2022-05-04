8a22e0b33f434b42ed7b1b36b991b1e63bf9e018
Manuel Pegoraro <pegoraro.m23@gmail.com>
2022-05-04 19:43:38 -0300

    Agregamos bootstrap a todo el proyecto y realizamos mejoras en responsive

--- trabajoIntegrador/src/app/app.module.ts
+++ trabajoIntegrador/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoComponent } from './components/logo/logo.component';
-import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
@@ -20,7 +19,6 @@ import { FooterComponent } from './components/footer/footer.component';
     AppComponent,
     HeaderComponent,
     LogoComponent,
-    SocialComponent,
     BannerComponent,
     AcercaDeComponent,
     EducacionComponent,
